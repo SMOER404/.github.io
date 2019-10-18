b188af0dc06430a4ffa5171eb8c08dd3703932be
Vlad <34536462+SMOER404@users.noreply.github.com>
2019-10-19 00:32:54 +0300

    Add quiz
    Add quiz

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
-$('.hall.owl-carousel').owlCarousel({
-    loop: true,
-    nav: true,
-    autoplay: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      600: {
-        items: 3
-      },
-      1000: {
-        items: 3
-      }
-    }
-});
-
+// Modal
 $(function () {
   $('.masonry__link').Chocolat();
 });
-
+// Carousel
+$('.hall.owl-carousel').owlCarousel({
+  loop: true,
+  nav: true,
+  autoplay: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 3
+    },
+    1000: {
+      items: 3
+    }
+  }
+});
 $('.reviews__carousel.owl-carousel').owlCarousel({
   loop: true,
   nav: true,
@@ -56,3 +56,120 @@ $('.clients__carousel.owl-carousel').owlCarousel({
     }
   }
 });
+// Modal
+$(".show").on("click", function(){
+  $(".mask").addClass("active");
+});
+function closeModal(){
+  $(".mask").removeClass("active");
+}
+$(".close, .mask").on("click", function(){
+  closeModal();
+});
+$(document).keyup(function(e) {
+  if (e.keyCode == 27) {
+    closeModal();
+  }
+});
+// QUIZ
+function startQuizz() {
+  var nextBtn = $('button[data-action="quiz__next"]');
+  var prevBtn = $('button[data-action="quiz__prev"]');
+  var submitBtn = $('button[data-action="quiz-submit"]');
+  var quizForm = $('#quiz__form');
+  var dots = $('.dots');
+  var quizItems = $('[data-quiz]');
+  var quizLength = quizItems.length;
+  var currentQuestion = 1;
+  var nextBtnText = nextBtn.text();
+
+  function checkButtons() {
+
+    function checkFirstQuestion() {
+      if(currentQuestion == 1) {
+          prevBtn.hide();
+      } else {
+          prevBtn.show();
+      }
+    }
+    function checkLastQuestion() {
+        if(currentQuestion == quizLength) {
+          $('.quiz__control').hide();
+          $('.quiz__header').hide();
+          // setTimeout(function () { 
+          //   location.reload();
+          // }, 30 * 1000);
+      } else {
+          submitBtn.hide();
+          nextBtn.show();
+      }
+    }
+    function checkSevenQuestion() {
+      if (currentQuestion == 7) {
+        nextBtnText = "";
+        nextBtn.text('Ввести контакты');
+        nextBtn.css('width', '160px');
+        $('.quiz__control-dots').hide();
+      }
+    }
+    function checkEightQuestion() {
+      if (currentQuestion == 8) {
+        nextBtnText = "";
+        nextBtn.text('Отправить заявку');
+        nextBtn.css('width', '160px');
+        $('.quiz__control-dots').hide();
+        $('.quiz__header').hide();
+      } 
+    }
+    checkFirstQuestion();
+    checkLastQuestion();
+    checkSevenQuestion();
+    checkEightQuestion();
+  }
+  
+  function nextQestion() {
+    if(currentQuestion !== 7 || currentQuestion !==8){
+      nextBtnText == "Далее";
+    }
+      currentQuestion++;
+      changeQuestion();
+  }
+  
+  function prevQestion() {
+      if(currentQuestion > 1) {
+        currentQuestion--;
+        changeQuestion();
+       }
+  }    
+  
+  function changeQuestion() {
+      checkButtons();
+      quizItems.hide();
+      dots.removeClass('active');
+      $('[step="'+ currentQuestion +'"]').addClass('active');
+      $('[data-quiz="'+ currentQuestion +'"]').css({'display': 'flex'});
+  }
+  
+  //общие обработчики для частей квиза
+  nextBtn.on('click', nextQestion);
+  prevBtn.on('click', prevQestion);
+  
+  //отправка формы
+  quizForm.on('submit', function() {
+      var th = $(this);
+      $.ajax({
+      type: "POST",
+      url: "mail.php", //Change
+      data: th.serialize()
+      }).done(function() {
+      alert("Спасибо за заявку, наш менеджер свяжется с Вами в ближайшее время!");
+      setTimeout(function() {
+      // Done Functions
+      th.trigger("reset");
+      }, 1000);
+      });
+      return false;
+  });
+  
+}
+startQuizz();
